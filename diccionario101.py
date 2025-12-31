9c9c659986f190fd2252cbbb6de0d83ed62d32dc
‘joses’ <josesallan@hotmail.com>
2025-12-31 17:30:27 +0100

    Actualizados ejercicio 1 al 4  utilizando diccionarios para los datos de los jugadores.  También he eliminado algún error de estilo

--- diccionario101.py
+++ diccionario101.py
@@ -4,17 +4,17 @@ titulares = {}
 
 # Añadimos cada uno de los jugadores, cada uno de los datos será a su vez un diccionario
 
-titulares[1] = {'posicion':'POR', 'nombre':'Iker Casillas', 'capitan':'X'}
-titulares[15] = {'posicion':'DEF', 'nombre':'Sergio Ramos'}
-titulares[3] = {'posicion':'DEF', 'nombre':'Gerard Piqué'}
-titulares[5] = {'posicion':'DEF', 'nombre':'Carles Puyo'}
-titulares[11] = {'posicion':'DEF', 'nombre':'Joan Capdevila'}
-titulares[14] = {'posicion':'MED', 'nombre':'Xabi Alonso'}
-titulares[16] = {'posicion':'MED', 'nombre':'Sergio Busquets'}
-titulares[8] = {'posicion':'MED', 'nombre':'Sergio Busquets'}
-titulares[18] = {'posicion':'MED', 'nombre':'Pedro Rodríguez'}
-titulares[6] = {'posicion':'MED', 'nombre':'Andrés Iniesta'}
-titulares[7] = {'posicion':'DEL', 'nombre':'David Villa'}
+titulares[1] = {'posicion': 'POR', 'nombre': 'Iker Casillas', 'capitan': 'X'}
+titulares[15] = {'posicion': 'DEF', 'nombre': 'Sergio Ramos'}
+titulares[3] = {'posicion': 'DEF', 'nombre': 'Gerard Piqué'}
+titulares[5] = {'posicion': 'DEF', 'nombre': 'Carles Puyo'}
+titulares[11] = {'posicion': 'DEF', 'nombre': 'Joan Capdevila'}
+titulares[14] = {'posicion': 'MED', 'nombre': 'Xabi Alonso'}
+titulares[16] = {'posicion': 'MED', 'nombre': 'Sergio Busquets'}
+titulares[8] = {'posicion': 'MED', 'nombre': 'Sergio Busquets'}
+titulares[18] = {'posicion': 'MED', 'nombre': 'Pedro Rodríguez'}
+titulares[6] = {'posicion': 'MED', 'nombre': 'Andrés Iniesta'}
+titulares[7] = {'posicion': 'DEL', 'nombre': 'David Villa'}
 
 # Mostramos el diccionario por pantalla
 print(titulares)

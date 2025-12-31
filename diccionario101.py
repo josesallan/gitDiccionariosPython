dd2f0d11f2e6d9ab923ff49cfe46d305d8022c68
‘joses’ <josesallan@hotmail.com>
2025-12-31 17:04:49 +0100

    Actualizados ejercicio 1 y 2 utilizando diccionarios para los datos de los jugadores

--- diccionario101.py
+++ diccionario101.py
@@ -1,14 +1,20 @@
 # Creamos el diccionario y añadimos los elementos
+# Partimos de un diccionario vacío
 titulares = {}
-titulares[1] = 'Casillas'
-titulares[3] = 'Pique'
-titulares[5] = 'Puyol'
-titulares[6] = 'Iniesta'
-titulares[7] = 'Villa'
-titulares[8] = 'Xavi Hernández'
-titulares[11] = 'Capdevila'
-titulares[14] = 'Xavi Alonso'
-titulares[15] = 'Ramos'
-titulares[16] = 'Busquets'
-titulares[18] = 'Pedrito'
+
+# Añadimos cada uno de los jugadores, cada uno de los datos será a su vez un diccionario
+
+titulares[1] = {'posicion':'POR', 'nombre':'Iker Casillas', 'capitan':'X'}
+titulares[15] = {'posicion':'DEF', 'nombre':'Sergio Ramos'}
+titulares[3] = {'posicion':'DEF', 'nombre':'Gerard Piqué'}
+titulares[5] = {'posicion':'DEF', 'nombre':'Carles Puyo'}
+titulares[11] = {'posicion':'DEF', 'nombre':'Joan Capdevila'}
+titulares[14] = {'posicion':'MED', 'nombre':'Xabi Alonso'}
+titulares[16] = {'posicion':'MED', 'nombre':'Sergio Busquets'}
+titulares[8] = {'posicion':'MED', 'nombre':'Sergio Busquets'}
+titulares[18] = {'posicion':'MED', 'nombre':'Pedro Rodríguez'}
+titulares[6] = {'posicion':'MED', 'nombre':'Andrés Iniesta'}
+titulares[7] = {'posicion':'DEL', 'nombre':'David Villa'}
+
+# Mostramos el diccionario por pantalla
 print(titulares)
